a0798c1c3596ae45f86a60db20f6759d1a63fcbc
Steven <stevenbaezfernandez@gmail.com>
2021-06-23 10:59:29 -0400

    oneline issue solved

--- search.php
+++ search.php
@@ -1,9 +1,6 @@
 <?php
 
 
-
-<?php
-
     header('Access-Control-Allow-Origin: *');
     header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
     header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
@@ -37,16 +34,6 @@
         echo($json);
 
     }
-    
-
-
-
-
-
-
-
-
-
 
 ?>
 
